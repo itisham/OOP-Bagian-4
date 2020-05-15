ef3064b0b923036fae570ee48747449a32520da4
itisham <61932014+itisham@users.noreply.github.com>
2020-05-15 22:42:47 +0700

    Add files via upload
    Polymorphism Menggunakan Interface

---
+++ Epson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Menggunakan_Interface
+{
+    public class Epson : IPrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("\nEpson display dimension : 10*11");
+        }
+        public void Print()
+        {
+            Console.WriteLine("\nEpson printer printing....");
+        }
+    }
+}
